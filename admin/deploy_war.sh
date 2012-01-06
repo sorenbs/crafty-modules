8d5e31072e95e22a6bf12f299a3105ea679b71db
sorenbs <sorenbs@gmail.com>
2012-01-06 22:53:16 +0100

    deploy in root

--- admin/deploy_war.sh
+++ admin/deploy_war.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /var/lib/jenkins/jobs/crafty-modules/workspace/target/scala-2.9.1/crafty-modules_2.9.1-0.1-SNAPSHOT.war /opt/jetty/webapps/crafty.war; service jetty restart;
+cp /var/lib/jenkins/jobs/crafty-modules/workspace/target/scala-2.9.1/crafty-modules_2.9.1-0.1-SNAPSHOT.war /opt/jetty/webapps/root.war; service jetty restart;
