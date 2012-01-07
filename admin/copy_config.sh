2456a5448421d40ea1a0600f96e4c5e2db6f629e
sorenbs <sorenbs@gmail.com>
2012-01-07 15:11:34 +0100

    fix props

--- admin/copy_config.sh
+++ admin/copy_config.sh
@@ -1,1 +1,1 @@
-cp /config/crafty-modules.props /var/lib/jenkins/jobs/crafty-modules/workspace/src/main/resources/props/default.props
+cp /config/crafty-modules.props /var/lib/jenkins/jobs/crafty-modules/workspace/src/main/resources/props/production.default.props
